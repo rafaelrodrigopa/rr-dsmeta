16f024af0130243f265da98d08bf07b76e75fdc3
Rafael Rodrigo <rafael.rodrigo.p.a@gmail.com>
2022-07-15 09:00:36 -0300

    Component - SalesCard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
 import Header from "./components/Header/Header"
 import NotificationButton from "./components/NotificationButton/NotificationButton"
+import SalesCard from "./components/SalesCard/SalesCard"
 
 function App() {
   return(
     <>
-      <h1>Olá mundo</h1>
       <Header />
+
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+
       <NotificationButton />
     </>
   )
