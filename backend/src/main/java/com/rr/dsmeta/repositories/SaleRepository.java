cc9fe8ca2a845e44039d28b06576f53aee05c30d
Rafael Rodrigo <rafael.rodrigo.p.a@gmail.com>
2022-07-16 10:54:54 -0300

    API Test

---
+++ backend/src/main/java/com/rr/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.rr.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rr.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
