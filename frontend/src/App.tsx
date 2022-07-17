52e290c10a975309707ea76aeb6f9dfbce4555c2
Rafael Rodrigo <rafael.rodrigo.p.a@gmail.com>
2022-07-17 20:26:26 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import Header from "./components/Header/Header"
 import NotificationButton from "./components/NotificationButton/NotificationButton"
 import SalesCard from "./components/SalesCard/SalesCard"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return(
     <>
+      <ToastContainer />
       <Header />
 
       <main>
