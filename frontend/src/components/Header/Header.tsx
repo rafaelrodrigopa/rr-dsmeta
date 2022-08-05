a33e96755f3f56dadbd2723d9c7c1cf9b08a25c9
Rafael Rodrigo <rafael.rodrigo.p.a@gmail.com>
2022-08-05 12:27:52 -0300

    Update Header.tsx

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -9,7 +9,7 @@ function Header() {
             <h1>DSMeta</h1>
             <p>
               Desenvolvido por
-              <a href="https://github.com/rafaelrodrigopa">rafaelrodrigopa</a>
+              <a href="https://github.com/rafaelrodrigopa"> rafaelrodrigopa</a>
             </p>
         </div>
     </header>
